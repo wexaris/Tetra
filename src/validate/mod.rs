3c9852196bac7c9966f7b29d8f9964c052181889
Wexaris <rainersvorza@gmail.com>
2024-05-29 07:59:12 +0300

    Remove entrypoint finder Expect entrypoint in the root module

--- src/validate/mod.rs
+++ src/validate/mod.rs
@@ -1,9 +1,7 @@
-mod entrypoint_finder;
 mod expr_desugar;
 mod expr_validate;
 mod visitor;
 
-pub use entrypoint_finder::*;
 pub use expr_desugar::*;
 pub use expr_validate::*;
 pub use visitor::*;
